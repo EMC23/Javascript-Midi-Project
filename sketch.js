6db1d93966dc05ad64bf6028e1a56bbc80f7a9f6
Rob Stocker <djtechbot@gmail.com>
2019-07-16 19:08:53 +0200

    Update sketch.js
    add indentation, remove dead code and comments

--- sketch.js
+++ sketch.js
@@ -2,44 +2,32 @@ function setup() {
   // put setup code here
     WebMidi.enable(function (err) {
         if (err) {
-                console.log("WebMidi could not be enabled.", err);
-                } else {
-
-                        console.log("WebMidi enabled!");
-                        console.log(WebMidi.inputs);
-                        //console.log(WebMidi.outputs);
-                        // Retrieve an input by name, id or index
-                        //var input = WebMidi.getInputByName("Launch Control");
-                        var input = WebMidi.getInputById("input-0");
-                   
-                        input.addListener('controlchange', "all",
-                            function (e) {
-                              console.log("Received 'controlchange' message.", e.data[2]);
-                         //        console.log("Received 'controlchange' message.", e);
-
-
-
-                            }
-                          );
-                          // Check for the presence of an event listener (n such cases, you cannot use anonymous functions).
-                          function test(e) { 
-                            console.log(e);
-                           }
-                        //  input.addListener('programchange', 12, test);
-                        input.addListener('controlchange', "all",function(e) {
-                            console.log("value: " + e.value);
-                            draw(e)
-                        });
-            }
+          console.log("WebMidi could not be enabled.", err);
+        } else {
+          console.log("WebMidi enabled!");
+          console.log(WebMidi.inputs);
+          //console.log(WebMidi.outputs);
+          var input = WebMidi.getInputById("input-0");
+          input.addListener('controlchange', "all",
+              function (e) {
+                console.log("Received 'controlchange' message.", e.data[2]);
+              }
+            );
+            // Check for the presence of an event listener (n such cases, you cannot use anonymous functions).
+            function test(e) { 
+              console.log(e);
+             }
+            //  input.addListener('programchange', 12, test);
+            input.addListener('controlchange', "all",function(e) {
+              console.log("value: " + e.value);
+              draw(e);
+            });
+          }
         }
     );
 }
 function draw(e) {
-
-if (typeof e !== 'undefined') {
-ellipse(50, 50, 80, e.value);
+  if (typeof e !== 'undefined') {
+    ellipse(50, 50, 80, e.value);
+  }
 }
-
-
-}
-
