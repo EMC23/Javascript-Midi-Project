8e3a3f87249a888658d5cc03c03ca28e95fc573e
left23 <lcdemc@gmail.com>
2019-11-14 18:25:03 +0100

    add some particles and background colour changes based on midi notes

--- sketch.js
+++ sketch.js
@@ -1,33 +1,82 @@
-function setup() {
-  // put setup code here
-    WebMidi.enable(function (err) {
-        if (err) {
-          console.log("WebMidi could not be enabled.", err);
-        } else {
-          console.log("WebMidi enabled!");
-          console.log(WebMidi.inputs);
-          //console.log(WebMidi.outputs);
-          var input = WebMidi.getInputById("input-0");
-          input.addListener('controlchange', "all",
-              function (e) {
-                console.log("Received 'controlchange' message.", e.data[2]);
-              }
-            );
-            // Check for the presence of an event listener (n such cases, you cannot use anonymous functions).
-            function test(e) { 
-              console.log(e);
-             }
-            //  input.addListener('programchange', 12, test);
-            input.addListener('controlchange', "all",function(e) {
-              console.log("value: " + e.value);
-              draw(e);
-            });
-          }
-        }
-    );
+// Variable which tell us what step of the game we're on.
+// We'll use this later when we parse noteOn/Off messages
+var currentStep = 0;
+var yPos = 0;
+
+// Request MIDI access
+if (navigator.requestMIDIAccess) {
+    console.log('This browser supports WebMIDI!');
+
+    navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
+
+} else {
+    console.log('WebMIDI is not supported in this browser.');
 }
-function draw(e) {
-  if (typeof e !== 'undefined') {
-    ellipse(50, 50, 80, e.value);
-  }
+
+// Function to run when requestMIDIAccess is successful
+function onMIDISuccess(midiAccess) {
+    var inputs = midiAccess.inputs;
+    var outputs = midiAccess.outputs;
+
+    // Attach MIDI event "listeners" to each input
+    for (var input of midiAccess.inputs.values()) {
+        input.onmidimessage = getMIDIMessage;
+    }
+}
+
+// Function to run when requestMIDIAccess fails
+function onMIDIFailure() {
+    console.log('Error: Could not access MIDI devices.');
+}
+
+// Function to parse the MIDI messages we receive
+// For this app, we're only concerned with the actual note value,
+// but we can parse for other information, as well
+function getMIDIMessage(message) {
+    var command = message.data[0];
+    var note = message.data[1];
+
+    console.log(command);
+    console.log(note);
+
+    var velocity = (message.data.length > 2) ? message.data[2] : 0; // a velocity value might not be included with a noteOff command
+
+    console.log(velocity);
+
+    switch (command) {
+        case 144: // note on
+            if (velocity > 0) {
+                noteOn(note, velocity);
+            } else {
+                noteOff(note);
+            }
+            break;
+    }
+}
+
+// Function to handle noteOn messages (ie. key is pressed)
+// Think of this like an 'onkeydown' event
+function noteOn(note, velocity) {
+    console.log('noteOn');
+
+      console.log(yPos);
+      // draw() loops forever, until stopped
+      yPos = yPos - 1;
+      if (yPos < 0) {
+        yPos = height;
+      }
+
+      stroke(255, 204, 0);
+      line(0, yPos, width, yPos);
+
+}
+
+// Function to handle noteOff messages (ie. key is released)
+// Think of this like an 'onkeyup' event
+function noteOff(note) {
+    console.log('noteOff');
+}
+
+function runSequence(sequence) {
+    console.log('sequence');
 }
